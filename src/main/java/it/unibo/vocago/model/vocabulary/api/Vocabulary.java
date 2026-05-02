476aa40057632d347e0bfe2b4a2eac48d93c8821
DuckLordSup <nick.rubinsmirnov@studio.unibo.it>
2026-05-02 16:08:23 +0200

    added Vocabulary interface

---
+++ src/main/java/it/unibo/vocago/model/vocabulary/api/Vocabulary.java
@@ -0,0 +1,16 @@
+package it.unibo.vocago.model.vocabulary.api;
+
+import java.util.List;
+
+public interface Vocabulary {
+
+    void addItem(VocabularyItem item);
+
+    void removeItem(VocabularyItem item);
+
+    List<VocabularyItem> getItems();
+
+    boolean isEmpty();
+
+    int size();
+}
