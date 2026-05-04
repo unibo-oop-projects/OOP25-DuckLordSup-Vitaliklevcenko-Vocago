d594e624a04970af8ad862b832b28b9c22a3023c
DuckLordSup <nick.rubinsmirnov@studio.unibo.it>
2026-05-04 17:56:44 +0200

    gradlew build passed successfully

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,17 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
 application {
     mainClass.set("it.unibo.vocago.main")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 tasks.shadowJar {
