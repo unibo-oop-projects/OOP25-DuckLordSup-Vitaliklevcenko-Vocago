2cf23deddebab3dbce9dcd43f06aebdaf83528c8
DuckLordSup <nick.rubinsmirnov@studio.unibo.it>
2026-06-20 19:58:37 +0200

    modified build.gradle.kts with university recommended plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     java
     application
     id("com.gradleup.shadow") version "9.3.1"
+    id("org.danilopianini.gradle-java-qa") version "1.164.0"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
 }
 
 repositories {
@@ -9,6 +16,9 @@ repositories {
 }
 
 dependencies {
+
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.8")
+
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
